75a971f0247da9c2c394e4a02f51a3583398e3f8
MShekow <s2mashek@uni-bonn.de>
2024-05-19 09:38:32 +0200

    Minor clean-ups for python-django

--- python-django/myproject/myproject/settings.py
+++ python-django/myproject/myproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,10 +79,19 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'CONN_MAX_AGE': 60  # avoid that the DB connection is always closed after every request
+    } if os.environ.get("POSTGRES") == "0" else {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': '5432',
+        'CONN_MAX_AGE': 60  # avoid that the DB connection is always closed after every request
     }
 }
 
-CONN_MAX_AGE = 60  # avoid that the DB connection is always closed after every request
+
 
 
 # Password validation
--- python-django/start-server.sh
+++ python-django/start-server.sh
@@ -8,4 +8,5 @@ uwsgi --chdir=/app/myproject \
     --master --pidfile=/tmp/project-master.pid \
     --processes=$WORKERS \
     --http=:8040 \
+    --listen 1024 \
     --disable-logging
